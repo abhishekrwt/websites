9aa1c12f75d0cfc8508f9b812f2f2005e2b7abc6
Abhishek Rawat <36125867+abhishekrwt@users.noreply.github.com>
2020-03-05 16:13:03 +0530

    Add files via upload

--- stellarMin/js/function.js
+++ stellarMin/js/function.js
@@ -1,5 +1,11 @@
+// ########################## image controls ##################################
+
 function readFile(input) {
     if (input.files && input.files[0]) {
+	if(input.files[0].length>1){
+		alert("Only 1 image can be uploaded !");
+		myFormReset();
+	}else{
         var reader = new FileReader();
         reader.onload = function (e) {
             //console.log(e.target.v);
@@ -15,13 +21,16 @@ function readFile(input) {
             }
         };
         reader.readAsDataURL(input.files[0]);
+	}
     }
 }
 
 function validateImage() {
     var formData = new FormData();
-
     var file = document.getElementById("img").files[0];
+    if(file.length >1){
+    	alert("asgdgfgj");
+    }
 
     formData.append("Filedata", file);
     var t = file.type.split('/').pop().toLowerCase();
@@ -61,6 +70,9 @@ function myFormReset() {
     document.getElementById("newCaption").innerHTML = "";
 }
 
+
+// ############################### Finding Caption ########################################
+
 $('#findCaption').bind('click', function () {
     if (input.files && input.files[0]) {
     var reader = new FileReader();
@@ -78,18 +90,93 @@ $('#findCaption').bind('click', function () {
 });
 
 
+function getPreText() {
+    var coolstr = [
+        "I think it is ",
+        "Is it ",
+        "My best guess is ",
+        "Oooweee, ",
+        "All I see is ",
+        "Hope I'm not wrong! "
+    ];
+    return coolstr[Math.floor(Math.random() * coolstr.length)];
+}
+
+
 $('#findCaptions').bind('click', function () {
+    if (window.screen.width < 480){
+        $('html,body').animate({
+            scrollTop: $("#valueCaption").offset().top
+        },
+        'slow');
+    }
     document.getElementById("newCaption").innerHTML =
         '<div class="loadAnimation"><span class="loader"><span class="loader-inner"></span></span><p style="margin : 100px 0 0 100px;" >Analyzing Image..</p></div>';
     setTimeout(function () {
         $(".loader-wrapper").fadeOut("slow");
         var data = "Two dogs are sitting in the park";
+        data = getPreText() + data;
         var htmlPreview = '<h1 class="display-1" style="color: #fc6b03; font-size: 36px" >Image Caption Generated</h1>' +
             '<p class="lead">' + data + '</p>';
         document.getElementById("newCaption").innerHTML = htmlPreview;
     }, 5000);
     
 });
+
+
+//################################# Web Camera ######################################
+Webcam.set({
+    width: 320,
+    height: 240,
+    image_format: 'jpeg',
+    jpeg_quality: 90
+});
+Webcam.attach('#my_camera'); 
+
+var shutter = new Audio();
+shutter.autoplay = true;
+shutter.src = navigator.userAgent.match(/Firefox/) ? 'shutter.ogg' : 'shutter.mp3';
+
+function take_snapshot() {
+    shutter.play();
+    // take snapshot and get image data
+    Webcam.snap(function (data_uri) {
+        // display results in page
+        document.getElementById('imgbodycam').innerHTML =
+            '<img src="' + data_uri + '"/>';
+        $('.showBlockcam').removeClass('d-none');
+        $('.hideBlockcam').addClass('d-none');
+    });
+}
+
+function myCamReset() {
+    document.getElementById("imgbodycam").innerHTML = "";
+    $('.hideBlockcam').removeClass('d-none');
+    $('.showBlockcam').addClass('d-none');
+    document.getElementById("newCaptioncam").innerHTML = "";
+}
+
+
+$('#findCaptioncam').bind('click', function () {
+    if (window.screen.width < 480) {
+        $('html,body').animate({
+            scrollTop: $("#valueCaptioncam").offset().top
+        },
+            'slow');
+    }
+    document.getElementById("newCaptioncam").innerHTML =
+        '<div class="loadAnimation"><span class="loader"><span class="loader-inner"></span></span><p style="margin : 100px 0 0 100px;" >Analyzing Image..</p></div>';
+    setTimeout(function () {
+        $(".loader-wrapper").fadeOut("slow");
+        var data = "Two dogs are sitting in the park";
+        data = getPreText() + data;
+        var htmlPreview = '<h1 class="display-1" style="color: #fc6b03; font-size: 36px" >Image Caption Generated</h1>' +
+            '<p class="lead">' + data + '</p>';
+        document.getElementById("newCaptioncam").innerHTML = htmlPreview;
+    }, 5000);
+
+});
+
 
 /*
  
